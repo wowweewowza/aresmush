a75e38d8b64318f32c9fdeb98b72e1cde544a500
wowweewowza <wowweewowza@gmail.com>
2026-07-23 19:43:52 -0700

    Trash bbpost update

--- plugins/postevent/helpers.rb
+++ plugins/postevent/helpers.rb
@@ -6,6 +6,8 @@ module AresMUSH
       msg = PostEvent.format_msg(event)
       author = Character.named(event.organizer_name)
       Forum.post(category_name, event.title, msg, author)
+      post = PostEvent.find_event_post_by_name(event)
+      post.update(event_id: event.id)
     end
 
     def self.format_msg(event)
@@ -13,20 +15,39 @@ module AresMUSH
     end
 
     def self.reply_to_forum_post(event)
+      Global.logger.debug "event: #{event} event id: #{event.id}"
       category_name = Global.read_config("postevent", "event_forum")
-      post = PostEvent.find_event_post(event)
+      post = PostEvent.find_event_post_by_id(event.id)
+      title_changed = PostEvent.find_event_post_by_name(event)
+      Global.logger.debug "#{post}"
       reply = "%xcUPDATED EVENT DETAILS%xn\n\n#{PostEvent.format_msg(event)}"
       author = Character.named(event.organizer_name)
       category = BbsBoard.find_one_by_name(category_name)
       if post == "error"
-        client = Global.client_monitor.find_client(author)
+        client = Global.client_monitor.find_web_client(author)
         return client.emit_failure "Cannot reply to event forum post with updates; you may want to do it manually. This happens when the event name has changed."
       else
         Forum.reply(category, post, author, reply)
+        if title_changed == "error"
+          post.update(subject: event.title+" ("+post.subject+")")
+        end
       end
     end
 
-    def self.find_event_post(event)
+    def self.find_event_post_by_id(event_id)
+      category_name = Global.read_config("postevent", "event_forum")
+      category = BbsBoard.find_one_by_name(category_name)
+      Global.logger.debug "#{event_id}"
+      post = category.bbs_posts.select { |post| post.event_id == event_id.to_i}.last
+      Global.logger.debug "#{post}"
+      if !post
+        return "error"
+      else
+        return post
+      end
+    end
+
+    def self.find_event_post_by_name(event)
       category_name = Global.read_config("postevent", "event_forum")
       category = BbsBoard.find_one_by_name(category_name)
       post = category.bbs_posts.select { |post| post.subject == event.title}.last
---
+++ plugins/postevent/public/postevent_bbspost.rb
@@ -0,0 +1,7 @@
+module AresMUSH  
+  class BbsPost
+      attribute :event_id, :type => DataType::Integer
+  
+  end
+  
+end
