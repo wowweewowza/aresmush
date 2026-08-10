35f9739bb9e50eaceec859d810390002ec3b495a
wowweewowza <wowweewowza@gmail.com>
2026-08-09 20:04:54 -0700

    Revert "Update fs3skills_char.rb"
    This reverts commit 3cdfdf42ce7e0fded3a8371f93d45aeec2f14f3a.

--- plugins/fs3skills/public/fs3skills_char.rb
+++ plugins/fs3skills/public/fs3skills_char.rb
@@ -5,7 +5,7 @@ module AresMUSH
     attribute :fs3_cookie_archive, :type => DataType::Integer, :default => 0
     attribute :fs3_scene_luck, :type => DataType::Hash, :default => {}
 
-    attribute :wow_focus, :type => DataType::Integer, :default => 0
+    attribute :wow_focus, :type => DataType::Interger, :default => 0
     
     collection :fs3_attributes, "AresMUSH::FS3Attribute"
     collection :fs3_action_skills, "AresMUSH::FS3ActionSkill"
