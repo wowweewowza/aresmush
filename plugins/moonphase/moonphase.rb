f0c0b6d7595e3d262b5892887eb9f68bd470348a
wowweewowza <wowweewowza@gmail.com>
2026-08-19 22:57:32 -0700

    Add Moonphase to game status

--- plugins/moonphase/moonphase.rb
+++ plugins/moonphase/moonphase.rb
@@ -1,7 +1,7 @@
 $:.unshift File.dirname(__FILE__)
 
 module AresMUSH
-     module MoonPhase
+  module MoonPhase
 
     def self.plugin_dir
       File.dirname(__FILE__)
@@ -20,7 +20,10 @@ module AresMUSH
     end
 
     def self.get_web_request_handler(request)
-      nil
+      case request.cmd
+      when "phaseName"
+        return PhaseNameRequestHandler
+      end
     end
     
   end
